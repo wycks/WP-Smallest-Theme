7ad3cf7ed746b15465ca2d771a7d46cd49f8d342
Wycks <bob.ellison@gmail.com>
2011-08-29 11:43:18 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+<html lang="en"> 
+<head>  
+<title><?php bloginfo('name'); ?></title>
+<meta name="description" content=<?php bloginfo('description'); ?> /> 
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+</head>    
+
+    
+
+
+
+<div class="container">
+    
+    <?php get_sidebar(); ?>
+    
+</div>
+
